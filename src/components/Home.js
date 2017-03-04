c13f6b2169fa0210d7ee3f991017032182453012
Tian Dong xue <forscape@163.com>
2017-03-04 15:40:38 +0800

    json

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Home extends React.Component{
+
+  render(){
+    return(
+      <div className='home'>
+          <div className='home-cover'>
+              <div >
+                  <h3>I'M forscape</h3>
+                  <p>FrontEnd Web Develogper </p>
+                  <a href='http://github.com/forscape1003'>github</a>
+              </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+
+export default Home;
