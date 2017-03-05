f0eeb5fb67d5146b3f0f0a9b16e6907d9e1a00fc
Tian Dong xue <forscape@163.com>
2017-03-05 14:50:46 +0800

    add

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {render} from 'react-dom';
 import App from './App.js';
 import Routers from './Routers'
 import 'highlight.js/styles/atom-one-light.css';
-// import './main.css';
+import './main.css';
 import './post.css';
 
 render(<Routers />,document.getElementById('root'));
